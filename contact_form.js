c111cf6b5c5d0df77860d8a8153163084cfde680
Valentyn Shybanov <vshybanov@twinfield.com>
2013-02-26 18:37:31 +0200

    Saving to localStorage only uid

--- contact_form.js
+++ contact_form.js
@@ -2,22 +2,26 @@ angular.module('gdgorgua',[])
 .controller('contactForm', function($scope,$http, $window) {
   if ($window.localStorage) {
 	var user = $window.localStorage.getItem('user');
-	if (user) $scope.user = JSON.parse(user);
+	if (user) {
+		  $http.get('api/participant.json',{params:{uid:user}}).then(function(r){ $scope.user = r.data});
+	}
   }
-  $http.get('api/participant.json').then(function(r){ $scope.user = r.data});
 
   $scope.submit = function() {
+    if (!$scope.contactForm.$valid) return;
+    console.log("Saving");
     $scope.showOk = false;
     $scope.saving = true;
     $scope.showError = false;
     var savedCb = function(r) {
        $scope.saving = false;
-       if (status != 200) {
+       if (r.status != 200) {
 	  $scope.showError = true;
        		console.log(r);
        } else {
 	  $scope.showOk = true;
-	  if ($window.localStorage) $window.localStorage.setItem('user', JSON.stringify($scope.user));
+	  var uid = r.data.uid;
+	  if ($window.localStorage) $window.localStorage.setItem('user', JSON.stringify(uid));
        }
     } 
     $http.post('api/participant.save.json', $scope.user).then(savedCb, savedCb);
